21fdf0fea8224285b56199949ce0237dafe03aff
Raj Kumar Singh <raj.niit@gmail.com>
2026-08-20 01:19:40 +0530

    react-basics: basic event handling

--- react-basics/src/App.jsx
+++ react-basics/src/App.jsx
@@ -1,18 +1,32 @@
 import { SingleTag, MultiTag, MultiTagFragment } from "./components/FirstComponent"
 import FoodMenu from "./components/RecipeListComponent"
+import FirstButton from "./components/EventHandlingComponent"
 import StyleComponent from "./components/StyleComponent"
 
 function App() { // a component
   return (
-    <div>
-      <SingleTag />
-      <MultiTag />
-      <MultiTagFragment />
-      <hr className="section" />
-      <StyleComponent />
-      <hr className="section" />
-      <FoodMenu />
-    </div>
+    <table>
+      <tr>
+        <td>
+          <div>
+            <SingleTag />
+            <hr className="section" />
+            <MultiTag />
+            <MultiTagFragment />
+            <hr className="section" />
+            <StyleComponent />
+            <hr className="section" />
+            <FoodMenu />
+          </div>
+        </td>
+        <td>
+          <div className="centered">
+            <FirstButton />
+            <hr className="section" />
+          </div>
+        </td>
+      </tr>
+    </table>
     
   )
 }
