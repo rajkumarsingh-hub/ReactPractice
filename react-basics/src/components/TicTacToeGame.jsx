5d1231c5c7c6283d7474b05561772503bd200b7b
Raj Kumar Singh <raj.niit@gmail.com>
2026-08-21 04:29:57 +0530

    react-basics: tic tac toe game 3x3

--- react-basics/src/components/TicTacToeGame.jsx
+++ react-basics/src/components/TicTacToeGame.jsx
@@ -1,6 +1,6 @@
-import { useState } from "react";
+import { useState } from 'react';
 
-function Square({value, onSquareClick}) {
+function Square({ value, onSquareClick }) {
     return (
         <button className="square" onClick={onSquareClick}>
             {value}
@@ -8,30 +8,26 @@ function Square({value, onSquareClick}) {
     );
 }
 
-export function Board({value}) {
-    const [xIsNext, setXIsNext] = useState(true);
-    const [squares, setSquares] = useState(Array(9).fill(null));
+function Board({ xIsNext, squares, onPlay, onReset }) {
     const winner = calculateWinner(squares);
     let status;
 
-    if (!squares.includes(null) && !winner) {
-        status = 'No Winner : Restart Game';
-    } else if(winner) {
-        status = 'Winner: ' + winner;
+    if (winner) {
+        status = 'Winner : ' + winner;
+    } else if (!squares.includes(null)) {
+        status = 'Draw : Restart Game';
     } else {
-        status = 'Next Player: ' + (xIsNext ? 'X' : 'O');
+        status = 'Player\'s Turn : ' + (xIsNext ? 'X' : 'O');
     }
 
     function handleClick(i) {
-        if(squares[i] || winner) {
+        if (winner || squares[i]) {
             return;
         }
-
-        const nextSquares = squares.slice(); // copying squares array to nextSquares array for immutability
+        const nextSquares = squares.slice();
         nextSquares[i] = xIsNext ? 'X' : 'O';
-        
-        setSquares(nextSquares);
-        setXIsNext(!xIsNext);
+
+        onPlay(nextSquares);
     }
 
     return (
@@ -52,7 +48,46 @@ export function Board({value}) {
                 <Square value={squares[7]} onSquareClick={() => handleClick(7)} />
                 <Square value={squares[8]} onSquareClick={() => handleClick(8)} />
             </div>
-        </> 
+            <div className="status">
+                {(winner || !squares.includes(null)) && (
+                    <button onClick={onReset}>
+                        Restart Game
+                    </button>
+                )}
+            </div>
+        </>
+    );
+}
+
+export default function TicTacToeGame() {
+    const [xIsNext, setXIsNext] = useState(true);
+    const [history, setHistory] = useState([Array(9).fill(null)]);
+    const currentSquares = history[history.length - 1];
+
+    function handlePlay(nextSquares) {
+        setHistory([...history, nextSquares]);
+        setXIsNext(!xIsNext);
+    }
+
+    function resetGame() {
+        setHistory([Array(9).fill(null)]);
+        setXIsNext(true);
+    }
+
+    return (
+        <div className="game">
+            <div className="game-board">
+                <Board 
+                    xIsNext={xIsNext} 
+                    squares={currentSquares} 
+                    onPlay={handlePlay}
+                    onReset={resetGame} />
+            </div>
+
+            <div className="game-info">
+                <ol>{/*TODO*/}</ol>
+            </div>
+        </div>
     );
 }
 
@@ -65,14 +100,14 @@ function calculateWinner(squares) {
         [1, 4, 7],
         [2, 5, 8],
         [0, 4, 8],
-        [2, 4, 6]
+        [2, 4, 6],
     ];
-
-    for(let i = 0; i<lines.length; i++) {
+    for (let i = 0; i < lines.length; i++) {
         const [a, b, c] = lines[i];
-        if(squares[a] && squares[a] === squares[b] && squares[a] === squares[c]) {
+        if (squares[a] && squares[a] === squares[b] && squares[a] === squares[c]) {
             return squares[a];
         }
     }
     return null;
 }
+
